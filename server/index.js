73a3da42e5e12d63302377f2de4cf6d226cc3cf6
Neelesh Bisht <neeleshbisht99@gmail.com>
2019-09-03 12:42:42 +0530

    inital_commit -2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express=require('express');
-const app=express();
-const port=4000;
+const cors=require('cors');
 const handle=require('./handlers');
+const bodyParser=require('body-parser');
+const app=express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const port=process.env.PORT;
+
 app.get('/',(req,res)=>res.json({hello:'world'}));
-
 app.use(handle.notFound);
-
 app.use(handle.errors);
+
 app.listen(port,console.log(`server started on port ${port}.`));
