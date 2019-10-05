29a81f99fa1f503f7e52518bb229ee0a9421ef9e
Neelesh Bisht <neeleshbisht99@gmail.com>
2019-10-05 12:34:55 +0530

    till video 10

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,15 @@
-import React from 'react';
-const App = () => <div>app works</div>
+import React,{Component} from 'react';
+import api from '../services/api';
+class App extends Component {
+   async componentDidMount(){
+       const result = await api.call('post','auth/login',{
+           username:'kelvin',
+           password:'password'
+       });
+       console.log(result);
+    }
+    render(){
+        return <div>App works</div>
+    }
+}
 export default App;
--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -22,9 +22,8 @@ exports.login=async (req,res,next)=>{
     try{
         const user=await db.User.findOne({username:req.body.username});
         const {id,username}=user;
-        const valid=await user.comparePassword(req.body.password);
-         
-        if(valid)
+        const valid= user.comparePasswords(req.body.password);  
+        if(valid) 
         {
             const token=jwt.sign({id,username},process.env.Secret);
             res.json({
